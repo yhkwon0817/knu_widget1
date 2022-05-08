c9e1b4eba657a443f366945f9ea58f373d8c98db
YHK <yhkwon0817@gmail.com>
2022-05-08 17:23:57 +0900

    fortune, news headlines, corona num cleared

---
+++ app/src/main/java/com/example/knu_widget/ClassTimeData.java
@@ -0,0 +1,45 @@
+package com.example.knu_widget;
+
+public class ClassTimeData {
+    public String day, classname, startTime, endTime;
+
+
+    public ClassTimeData(String day,String classname,String startTime,String endTime)
+    {
+        this.day = day;
+        this.classname = classname;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+}
--- app/src/main/java/com/example/knu_widget/GetData.java
+++ app/src/main/java/com/example/knu_widget/GetData.java
@@ -3,12 +3,109 @@ package com.example.knu_widget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.jsoup.select.Evaluator;
+
+import java.util.ArrayList;
+
+
 
 public class GetData extends AppCompatActivity {
 
+    private Spinner spinner1,spinner2;
+    ArrayList<ClassTimeData> classInfo;
+    EditText name,st_time,end_time;
+    ArrayAdapter<String> arrayAdapter,dayAdapter;
+    String selected_info1;
+    Button btn1,btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_data);
+
+        classInfo = new ArrayList<ClassTimeData>();
+        spinner1 = (Spinner)findViewById(R.id.spinner1);
+        spinner2 = (Spinner)findViewById(R.id.spinner2);
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+        name = (EditText) findViewById(R.id.name);
+        st_time = (EditText) findViewById(R.id.st_time);
+        end_time = (EditText)findViewById(R.id.end_time);
+        ListView listView = (ListView) findViewById(R.id.listivew);
+        MyAdapter myAdapter = new MyAdapter(this,classInfo);
+        ArrayList arrayList = new ArrayList<>();
+        ArrayList day = new ArrayList<>();
+        arrayList.add("쥐띠");
+        arrayList.add("소띠");
+        arrayList.add("호랑이띠");
+        arrayList.add("토끼띠");
+        arrayList.add("용띠");
+        arrayList.add("뱀띠");
+        arrayList.add("말띠");
+        arrayList.add("양띠");
+        arrayList.add("원숭이띠");
+        arrayList.add("닭띠");
+        arrayList.add("개띠");
+        arrayList.add("돼지띠");
+        day.add("월요일");
+        day.add("화요일");
+        day.add("수요일");
+        day.add("목요일");
+        day.add("금요일");
+
+        arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, arrayList);
+
+        spinner1 = (Spinner)findViewById(R.id.spinner1);
+        spinner1.setAdapter(arrayAdapter);
+
+        dayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, day);
+
+        spinner2 = (Spinner)findViewById(R.id.spinner2);
+        spinner2.setAdapter(dayAdapter);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                selected_info1 = spinner1.getSelectedItem().toString();
+                Log.e("###",selected_info1);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(getApplicationContext(),"띠를 골라 주십시오",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        listView.setAdapter(myAdapter);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                classInfo.add(new ClassTimeData(spinner2.getSelectedItem().toString(),name.getText().toString(),st_time.getText().toString(),end_time.getText().toString()));
+                listView.setAdapter(myAdapter);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                classInfo.remove(classInfo.size()-1);
+                listView.setAdapter(myAdapter);
+            }
+        });
+
+
+
     }
 }
