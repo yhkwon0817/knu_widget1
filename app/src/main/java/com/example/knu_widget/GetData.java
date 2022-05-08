e8d7147307a23996ae03f61b722e73233e60cf63
YHK <yhkwon0817@gmail.com>
2022-05-08 14:21:20 +0900

    fortune, news headlines, corona num cleared

---
+++ app/src/main/java/com/example/knu_widget/GetData.java
@@ -0,0 +1,14 @@
+package com.example.knu_widget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_data);
+    }
+}
